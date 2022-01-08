e4d760a3c0182f3f05128440244a2ca8a44ce628
ZnoGouDj <znogoud3@gmail.com>
2022-01-09 01:12:28 +0300

    add login method

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -24,7 +24,19 @@ class UserController {
     return res.json({ token });
   }
 
-  async login(req, res) {}
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.internal('User can not be found'));
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal('Wrong password'));
+    }
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
 
   async check(req, res, next) {
     const { id } = req.query;
