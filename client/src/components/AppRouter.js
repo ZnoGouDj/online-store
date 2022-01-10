515c37df26de6931a95962e598b526bad96bb92a
ZnoGouDj <znogoud3@gmail.com>
2022-01-10 13:22:58 +0300

    add navigation & routing

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { authRoutes, publicRoutes } from '../routes';
+import { SHOP_ROUTE } from '../utils/consts';
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  const isAuth = false;
+  return (
+    <Routes>
+      {isAuth && authRoutes.map(({ path, Component }) => <Route key={path} path={path} component={Component} exact />)}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} component={Component} exact />
+      ))}
+      <Navigate to={SHOP_ROUTE} />
+    </Routes>
+  );
 };
 
 export default AppRouter;
